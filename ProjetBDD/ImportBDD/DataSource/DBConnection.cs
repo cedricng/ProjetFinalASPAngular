5e04fb70f38f54e1e84d65f61cdc4b872ead2e16
AbdouDIOP41 <57726810+AbdouDIOP41@users.noreply.github.com>
2024-01-27 00:22:16 +0100

    program for import bdd

---
+++ ProjetBDD/ImportBDD/DataSource/DBConnection.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportBDD.DataSource
+{
+    class DBConnection
+    {
+        public static SqlConnection Connection()
+        {
+            string connectionString = @"Data Source=DESKTOP-D82CEGJ;Initial Catalog=projet-hopital-db;Integrated Security=True";
+            return new SqlConnection(connectionString);
+        }
+        public static void Connecting()
+        {
+            try
+            {
+                SqlConnection connection = Connection();
+                connection.Open();
+                Console.WriteLine("successful connection");
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("connection failed !");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ProjetBDD/ImportBDD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using ImportBDD.DataSource;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //DBConnection.Connecting();
+        DBCreate.CreerTableBDD();
+
+    }
+}
