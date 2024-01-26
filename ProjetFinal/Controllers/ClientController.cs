54ac6256ad6af829d4fde81c4066d88573073072
cedricng <ngoupandecedric@hotmail.fr>
2024-01-26 16:16:48 +0100

    login backend

---
+++ ProjetFinal/Controllers/ClientController.cs
@@ -0,0 +1,63 @@
+﻿using ProjetFinal.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ProjetFinal.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ClientController : ApiController
+    {
+        // GET: api/Client
+        public IEnumerable<clients> Get()
+        {
+            return new DaoClient().FindAll();
+
+        }
+
+
+
+
+
+        // GET: api/Client/?login=pseudo
+        public clients Get(string login)
+        {
+            return new DaoClient().FindByLogin(login);
+        }
+
+        /*// GET: api/Client/
+        public clients GetPost([FromBody]string login)
+        {
+            return new DaoClient().FindByLogin(login);
+        }*/
+
+        // POST: api/Client
+        public void Post([FromBody]clients value)
+        {
+            new DaoClient().Create(value);
+
+        }
+
+        // PUT: api/Client
+        public void Put([FromBody]clients value)
+        {
+            new DaoClient().Update(value);
+        }
+
+        // DELETE: api/Client/?login=pseudo
+        public void Delete(string login)
+        {
+            new DaoClient().Delete(login);
+        }
+
+        /*// DELETE: api/Client/
+        public void DeletePost([FromBody]string login)
+        {
+            new DaoClient().Delete(login);
+        }*/
+    }
+}
---
+++ ProjetFinal/DAL/DaoClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetFinal.DAL
+{
+    public class DaoClient
+    {
+        public List<clients> FindAll()
+        {
+            projetfinalEntities context = new projetfinalEntities();
+            return context.clients.ToList<clients>();
+        }
+        public clients FindByLogin(string login)
+        {
+            projetfinalEntities context = new projetfinalEntities();
+            return context.clients.Find(login);
+        }
+        public clients Create(clients p)
+        {
+            projetfinalEntities context = new projetfinalEntities();
+            context.clients.Add(p);
+            context.SaveChanges();
+            return p;
+        }
+
+
+
+
+
+        public void Delete(string login)
+        {
+            projetfinalEntities context = new projetfinalEntities();
+            clients p = context.clients.Find(login);
+            context.clients.Remove(p);
+            context.SaveChanges();
+        }
+        public void Update(clients p)
+        {
+            projetfinalEntities context = new projetfinalEntities();
+            context.Entry(p).State = EntityState.Modified;
+            context.SaveChanges();
+
+
+
+        }
+    }
+}
