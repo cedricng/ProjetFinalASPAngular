bf50a51b8163d9478c7cffe4dfbf01d67cfbca65
cedricng <ngoupandecedric@hotmail.fr>
2024-01-26 11:52:32 +0100

    first commit

---
+++ ProjetFinal/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjetFinal.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ProjetFinal/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjetFinal.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
